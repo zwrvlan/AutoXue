6ce367454c4e974a48f9f0878b3d7136e868f85f
Syrone Wong <wong.syrone@gmail.com>
2019-11-27 19:12:19 +0800

    使用安全随机数发生器

--- xuexi/__main__.py
+++ xuexi/__main__.py
@@ -8,10 +8,10 @@
 @time: 2019-10-26(星期六) 10:22
 @Copyright © 2019. All rights reserved.
 '''
-import random
 import time
 from . import App
 from .unit import logger
+from xuexi import SecureRandom
 
 
 app = App()
